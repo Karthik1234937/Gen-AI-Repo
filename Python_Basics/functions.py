a2182e601da2e15a2bab25cedfcd9e8d575e808c
karthik <kallamkarthikreddy25@gmail.com>
2026-06-01 07:58:27 +0530

    functions

--- Python_Basics/functions.py
+++ Python_Basics/functions.py
@@ -145,9 +145,118 @@
 #     pass
 
 
-def test_func(**param1):
-    print(param1)
+# def test_func(**param1):
+#     print(param1) 
+# test_func(name="John",age=30,city="New York")
 
-test_func(name="Samba",sub="Gen AI")
+#name - non default parameter
+#course - default parameter
+#skills - variable length (tuple)
+#details - variable length (dict)
+#def test_fact(name,
+#             course="Python",
+#            *skills,
+#           **details):
+#    print(name)
+#   print(course)
+#   print(skills)
+#   print(details)
+
+#test_fact("Karthik", "Gen AI", "ML", "DL", "NLP", city="Hydrabad")
+
+
+#normal --> default --> variable length (tuple) --> variable length (dict)
+
+
+# Lambda functions (or) Anonymous functions - functions without name
+# "lambda" is the keyword used to declare the lambda functions
+
+
+#def add(num1,num2):
+#    return num1 + num2
+#print(add(200,100))
+
+#add = lambda num1,num2:num1+num2
+#print(add(200,100))
+
+
+#find the bigger number
+#big = lambda num1,num2: num1 if num1>num2 else num2
+#print(big(200,100))
+
+#evwn or odd
+#even_odd = lambda num1: "Even" if num1%2==0 else "Odd" 
+#print(even_odd(10))
+
+
+# str = "Hello" len(str) --> 5
+
+#res = lambda str: len(str)
+#print(res("Python"))
+
+"""
+            map() function
+ [1,2,3,4,5] --> [10,20,30,40,50]
+    map() - predefined function, used to manipulate "all list elements"
+                            filter()
+    [100,200,300,400,500] >300 --> 400,500
+ """
+
+#res=list(map(lambda num1:num1*10,[1,2,3,4,5]))
+#print(res)
+
+#print(list(filter(lambda param1:param1>300,[100,200,300,400,500])))
+
+#[1,2,3,4,5] --> even elements
+#print(list(filter(lambda num1:num1%2==0,[1,2,3,4,5])))
+
+#sorted() - used to sort the list 
+#names = ["Karthik","ravi","A","python"]
+#print(sorted(names,key=lambda x:len(x))) #['A', 'ravi', 'python', 'Karthik']
+
+#sort based on marks
+#students = [
+#    ("std1",85),
+#    ("std2",90),
+#    ("std3",80)
+#]
+#print(sorted(students,key=lambda std:std[1]))
+
+#reduce() - used to perfom sum of elements
+# [1,2,3,4,5] --> [15]
+#from functools import reduce
+#print(reduce(lambda num1,num2:num1+num2,[1,2,3,4,5]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
